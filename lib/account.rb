c4604912dc50b549aabf33b30306555bb2256ddd
Cynthia Fu <yinnycfu@gmail.com>
2021-08-02 17:00:52 +0100

    test: deposit money, deposit multiple times

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,9 @@
 class Account
+  def initialize
+    @balance = 0
+  end
 
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,14 @@
 require 'account'
 
 describe Account do
-  
+  context '#deposit' do
+    it "can deposit 50 then return the balance" do
+      expect(subject.deposit(50)).to eq(50)
+    end 
+
+    it "can deposit multiple times" do
+      4.times{ subject.deposit(50) }
+      expect(subject.deposit(50)).to eq(250)
+    end
+  end
 end
