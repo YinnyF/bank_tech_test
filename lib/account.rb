65ebd35862f14048f0a7fe929e0d6fa999779e0a
Cynthia Fu <yinnycfu@gmail.com>
2021-08-02 18:14:58 +0100

    test: deposit money creates a transaction

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,19 @@
+require 'transaction'
+
 class Account
-  def initialize
+  def initialize(transaction_class = Transaction)
     @balance = 0
+    @transaction_class = transaction_class
   end
 
   def deposit(amount)
+    create_transaction
     @balance += amount
+  end
+
+  private
+
+  def create_transaction()
+    @transaction_class.new
   end
 end
--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -1,14 +1,27 @@
 require 'account'
 
 describe Account do
+  let(:transaction_class_double) { double(:Transaction) }
+  let(:transaction_double) { double(:transaction) }
+  subject { described_class.new(transaction_class_double) }
+
+  before do
+    allow(transaction_class_double).to receive(:new).and_return(transaction_double)
+  end
+
   context '#deposit' do
     it "can deposit 50 then return the balance" do
       expect(subject.deposit(50)).to eq(50)
     end 
 
     it "can deposit multiple times" do
-      4.times{ subject.deposit(50) }
+      4.times { subject.deposit(50) }
       expect(subject.deposit(50)).to eq(250)
+    end
+
+    it "creates a transaction" do
+      expect(transaction_class_double).to receive(:new).once
+      subject.deposit(50)
     end
   end
 end
