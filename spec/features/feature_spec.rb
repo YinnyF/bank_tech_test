908899dcd6db68679c76ee2005ef5f827bd1d5dc
Cynthia Fu <yinnycfu@gmail.com>
2021-08-02 18:35:13 +0100

    feat: withdraw money

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,6 +3,12 @@ require 'account'
 describe 'Bank' do
   it 'has an account that you can deposit money' do
     account = Account.new
-    expect(account.deposit(50)).to eq (50)
+    expect(account.deposit(50)).to eq 50
+  end
+
+  it 'has an account that you can withdraw money from' do
+    account = Account.new
+    account.deposit(1000)
+    expect(account.withdraw(50)).to eq 950
   end
 end
