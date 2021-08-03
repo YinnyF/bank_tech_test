ffd65b466ce66d499aa092c89957fbd4fb454483
Cynthia Fu <yinnycfu@gmail.com>
2021-08-03 14:19:23 +0100

    test: statement prints out transactions

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,44 @@
 class Statement
   HEADER = "date || credit || debit || balance"
 
-  def print
+  def print(transactions = [])
+    print_header()
+    print_transactions(transactions)    
+  end
+
+  private
+
+  def print_header
     puts HEADER
   end
-  
+
+  def stringify_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+
+  def stringify_amount(amount)
+    return "\b" if amount.nil?
+
+    '%.2f' % amount
+  end
+
+  def sort_transactions(transactions)
+    transactions.sort_by! { |transaction| transaction.date }.reverse
+  end
+
+  def create_transaction_line(transaction)
+    [stringify_date(transaction.date), 
+      stringify_amount(transaction.credit),
+      stringify_amount(transaction.debit),
+      stringify_amount(transaction.running_balance)
+    ]
+  end
+
+  def print_transactions(transactions)
+    transactions = sort_transactions(transactions)
+    transactions.each { |transaction|
+      line = create_transaction_line(transaction)
+      puts line.join(" || ")
+    }
+  end
 end
--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -1,7 +1,33 @@
 require 'statement'
 
 describe Statement do
-  it 'can print the header' do
-    expect { subject.print }.to output("date || credit || debit || balance\n").to_stdout
+  let(:header) { "date || credit || debit || balance\n" }
+
+  context 'without any transactions' do
+    it 'can print the header' do
+      expect { subject.print }.to output(header).to_stdout
+    end
+  end
+
+  context 'with transactions' do
+    let(:transaction_1) { double('transaction', date: Time.local(2012, 01, 10), debit: nil, credit: 1000, running_balance: 1000) }
+    let(:transaction_2) { double('transaction', date: Time.local(2012, 01, 13), debit: nil, credit: 2000, running_balance: 3000) }
+    let(:transaction_3) { double('transaction', date: Time.local(2012, 01, 14), debit: 500, credit: nil, running_balance: 2500) }
+
+    it { is_expected.to respond_to(:print).with(1).argument }
+
+    it 'can print the statement when given a transaction' do
+      transactions = [transaction_1]
+      statement = header + "10/01/2012 || 1000.00 || \b || 1000.00\n"
+      expect { subject.print(transactions) }.to output(statement).to_stdout
+    end
+
+    it 'can print the statement in reverse chronolocal order when given an array of transactions' do
+      transactions = [transaction_1, transaction_2, transaction_3]
+      # quick fix for backspace escape character - please revise later.
+      statement = header + "14/01/2012 || \b || 500.00 || 2500.00\n13/01/2012 || 2000.00 || \b || 3000.00\n10/01/2012 || 1000.00 || \b || 1000.00\n"
+      expect { subject.print(transactions) }.to output(statement).to_stdout
+    end
+
   end
 end
