7972dee29f8abf07124845e82faa33cae4c156c5
Cynthia Fu <yinnycfu@gmail.com>
2021-08-03 11:21:54 +0100

    test: (pending) print the statement

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,4 +1,5 @@
 require 'account'
+require 'timecop'
 
 describe 'Bank' do
   it 'has an account that you can deposit money' do
@@ -10,5 +11,24 @@ describe 'Bank' do
     account = Account.new
     account.deposit(1000)
     expect(account.withdraw(50)).to eq 950
+  end
+
+  xit "can print the statement" do
+    account = Account.new
+
+    Timecop.freeze(Time.local(2012, 01, 10)) do
+      account.deposit(1000)
+    end
+
+    Timecop.freeze(Time.local(2012, 01, 13)) do
+      account.deposit(2000)
+    end
+
+    Timecop.freeze(Time.local(2012, 01, 14)) do
+      account.withdraw(500)
+    end
+
+    statement = "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00"
+    expect(account.print_statement).to eq(statement)
   end
 end
