e397a14a06a32f0fdb407486018d380124e1afa3
Cynthia Fu <yinnycfu@gmail.com>
2021-08-03 15:17:33 +0100

    test: transactions are initialised with a date

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction
+  attr_reader :date
   
+  def initialize
+    @date = Time.now
+  end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -1,5 +1,15 @@
 require 'transaction'
+require 'timecop'
 
 describe Transaction do
+  it "has a date when initialised" do
+    Timecop.freeze(Time.local(2012, 01, 10)) do
+      expect(subject.date).to eq(Time.local(2012, 01, 10))
+    end
+  end
   
 end
+
+# let(:transaction_1) { double('transaction', date: Time.local(2012, 01, 10), debit: nil, credit: 1000, running_balance: 1000) }
+# let(:transaction_2) { double('transaction', date: Time.local(2012, 01, 13), debit: nil, credit: 2000, running_balance: 3000) }
+# let(:transaction_3) { double('transaction', date: Time.local(2012, 01, 14), debit: 500, credit: nil, running_balance: 2500) }
